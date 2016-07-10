2ff4f17ae29cbab8d5277067e635d56d397875b3
SaranyaPadmanaban <saranya.kp14@gmail.com>
2016-07-10 14:55:03 +0530

    Create vowelconst.c

---
+++ vowelconst.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+scanf("%c",&ch);
+if(ch=='a'||ch=='A'||ch=='b'||ch=='B'||ch=='c'||ch=='C'||ch=='d'||ch=='D'||ch=='e'||ch=='E')
+printf("vowel");
+else
+printf("consonant");
+return 0;
+}
